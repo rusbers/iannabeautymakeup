84bfc970c7d9d6fdf75399f345d82ed9be65b3b1
Ruslan <ruslan.berendeev@gmail.com>
2025-05-03 20:16:03 +0700

    hero image

--- components/blocks/hero.tsx
+++ components/blocks/hero.tsx
@@ -63,7 +63,6 @@ export function HeroBlock({
         <>
           <SanityImage
             aria-hidden
-            loading="eager"
             priority
             asset={background}
             fill
